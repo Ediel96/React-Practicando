1932bb92e8edd20f8fedcfdc2c37cede1d5b1fe2
Hamilton <h.amiltone.diel@gmail.com>
2022-04-15 17:34:04 -0500

    useEffect simpleForm

---
+++ 06-hook-app/src/components/01-useState/CounterWithCustomHook.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useCounter } from '../../hook/useCounter';
+
+export const CounterWithCustomHook = () => {
+
+  const {state, increment , decrement, reset} = useCounter( 100 );
+ 
+  
+  return (
+    <>
+        <h1>Counter with Hook:{state}</h1>
+        <button className='btn btn-primary m-2' onClick={ () => increment(2)}>+1</button>
+        <button className='btn btn-primary m-2' onClick={ () => reset(10)}>Reset</button>
+        <button className='btn btn-primary m-2' onClick={ () => decrement(2)}>-1</button>
+    </>
+  )
+}
--- 06-hook-app/src/index.js
+++ 06-hook-app/src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './components/01-useState/CounterApp';
+
+
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+
+import { SimpleForm } from './components/02-useEffect/SimpleForm';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
-    <CounterApp />
+    <div className='container'>
+      <SimpleForm />
+    </div>
   </>
 );
 
