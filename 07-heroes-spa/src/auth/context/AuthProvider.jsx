a60422a5257f8891ae3183f55cb492f7926de78b
Eddie <h.amiltone.diel@gmail.com>
2022-09-19 00:53:09 -0500

    falto mas

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -1,4 +1,5 @@
 import React, { useReducer } from 'react'
+import { types } from '../types/types'
 import { AuthContext } from './AuthContext'
 import { authReducer } from './authReducer'
 
@@ -10,6 +11,20 @@ const initialSate = {
 export const AuthProvider = ({ children }) => {
 
   const  [ authState, dispatch ] = useReducer( authReducer, initialSate );
+
+  const login = (name = '') => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: 'ABC',
+        name: name
+      }
+    }
+
+    dispatch(action);
+  }
+
+  
 
   return (
     <AuthContext.Provider value={{ }}>
