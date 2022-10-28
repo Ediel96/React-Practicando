55e849c41ba426617b6c39b99449369300341d3e
DarkEdiel96 <39193308+Ediel96@users.noreply.github.com>
2022-10-28 00:39:07 -0700

    configuracion de mi calendario

--- 10-calendar/src/calendar/pages/CalendarPage.jsx
+++ 10-calendar/src/calendar/pages/CalendarPage.jsx
@@ -1,6 +1,50 @@
+import { Navbar } from "../"
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+
+import enUS from 'date-fns/locale/en-US'
+import {addHours, format, parse, startOfWeek, getDay} from 'date-fns'
+
+
+const locales = {
+  'en-US': enUS,
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+const events = [{
+    title : 'Cumpleanos del jefe',
+    note: 'Hay que comprar una torta',
+    start: new Date(),
+    end: addHours( new Date(), 2),
+    bgColor: '#fafafa',
+    user  : {
+        _id:'123',
+        name: 'Fernando'
+    }
+}]
 
 export const CalendarPage = () => {
   return (
-    <div>CalendarPage</div>
+    <>
+      <Navbar/>
+
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px)' }}
+      />
+
+
+    </>
   )
 }
