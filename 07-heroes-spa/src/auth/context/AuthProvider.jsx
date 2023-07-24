6d8e592c2bcf4bcf7bacb67dd46f4e24c22b792e
Eddie <h.amiltone.diel@gmail.com>
2023-07-23 23:18:27 -0500

    tengo un error

---
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+import { useReducer } from "react"
+import { AuthContext } from "./AuthContext"
+import { authReducer } from "./authReducer";
+import { types } from "../types/types";
+
+
+const initialState = {
+    logged: false,
+}
+
+// eslint-disable-next-line react/prop-types
+export const AuthProvider= ({ children }) => {
+
+    const [ authState, dispatch] = useReducer( authReducer,  initialState);
+
+    const login = ( name = '' ) => {
+
+        const action = { type: types.login, payload: {
+            id: 'ABC',
+            name: name
+        } }
+
+        dispatch(action);
+      }
+
+    return (
+        <AuthContext.Provider value={{
+            ...authState,
+            login: login
+        }}>
+            { children }
+        </AuthContext.Provider>
+    )
+}
